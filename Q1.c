9fed95101007660c030ef1cbe7ce378f0fe5ca32
Nuno <up201405774@fe.up.pt>
2020-05-01 03:51:20 +0100

    acho que o main é isso mas ainda tenho de perceber a função un_thrd_handler()

--- Q1.c
+++ Q1.c
@@ -6,7 +6,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
-#include <stdlib.h>
 
 #include "utils.h"
 
@@ -19,5 +18,32 @@ int main(int argc, char* argv[]){
         printf("Usage: %s <-t nsec>  fifoname\n", argv[0]);
         exit(1);
     }
-    return 0;
+	
+	
+    server_ts server = server_handler(argv);
+
+    if (mkfifo(server.fifoname, /* MASK */) < 0) {
+        printf("Error on creating FIFO file \n");
+        exit(1);
+    }
+	
+	
+    
+
+    long int time_out = server.secs * 1000;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
+	int file = open(server.fifoname, O_RDONLY | O_NONBLOCK);
+    
+	while (time_out > time_ms()) {
+        server_ts request;
+		pthread_t t_pid;
+        while (read(fd, &request, sizeof(server_ts)) <= 0) {
+            usleep(50000);
+        }
+        pthread_create(&t_pid, NULL,/* i think its a different -> un_thrd_handler(server) */, &request);
+    }
+
+    close(file);
+
+    exit(0);
 }
